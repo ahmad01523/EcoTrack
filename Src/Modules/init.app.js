9d9ffc26a4107ed4d632783db320002e530dd09b
Ahmad Darawsheh <hmod00asad@gmail.com>
2023-12-25 15:45:59 +0200

    commit init

--- Src/Modules/init.app.js
+++ Src/Modules/init.app.js
@@ -3,7 +3,7 @@ import dataRouter from "./DataCollection/DatacCollection.router.js";
 import authRouter from "./Auth/Auth.router.js";
 import EnvironmentalAlert from "./EnvironmentalAlert/EnvironmentalAlert.router.js";
 import reportRouter from "./CommunityReporting/CommunityReporting.router.js";
-//import userProfile from "./UserProfile/UserProfile.router.js";
+
 
 const initApp = (app, express) => {
   app.use(express.json());
@@ -12,7 +12,7 @@ const initApp = (app, express) => {
   app.use("/dataCollection", dataRouter);
   app.use("/EnvironmentalAlert", EnvironmentalAlert);
   app.use ("/communityreport",reportRouter);
-  //app.use("/userProfile", userProfile )
+
   app.use("*", (req, res) => {
     return res.json({ message: "Page not found" });
   });
