f1a977567a5242a92066f8795a0a594b7fc15a31
Ahmad Darawsheh <hmod00asad@gmail.com>
2023-12-25 15:40:03 +0200

    report router in init.js

--- Src/Modules/init.app.js
+++ Src/Modules/init.app.js
@@ -2,7 +2,7 @@ import connectDB from "../../DB/connection.js";
 import dataRouter from "./DataCollection/DatacCollection.router.js";
 import authRouter from "./Auth/Auth.router.js";
 import EnvironmentalAlert from "./EnvironmentalAlert/EnvironmentalAlert.router.js";
-
+import reportRouter from "./CommunityReporting/CommunityReporting.router.js";
 //import userProfile from "./UserProfile/UserProfile.router.js";
 
 const initApp = (app, express) => {
@@ -11,7 +11,7 @@ const initApp = (app, express) => {
   app.use("/auth", authRouter);
   app.use("/dataCollection", dataRouter);
   app.use("/EnvironmentalAlert", EnvironmentalAlert);
-
+  app.use ("/communityreport",reportRouter);
   //app.use("/userProfile", userProfile )
   app.use("*", (req, res) => {
     return res.json({ message: "Page not found" });
